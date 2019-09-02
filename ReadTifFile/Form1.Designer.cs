0a9ee7cd5a4275c8a20edef42db67084249a35fe
mai ha an <maihaan24@gmail.com>
2019-09-03 01:21:25 +0700

    Project
    Read raster data from tif file

---
+++ ReadTifFile/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ReadTifFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.tbLatLon = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(13, 13);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btBrowse.TabIndex = 0;
+            this.btBrowse.Text = "Browse";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lat - Lon";
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(94, 16);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(242, 20);
+            this.tbPath.TabIndex = 2;
+            // 
+            // tbLatLon
+            // 
+            this.tbLatLon.Location = new System.Drawing.Point(15, 68);
+            this.tbLatLon.Name = "tbLatLon";
+            this.tbLatLon.Size = new System.Drawing.Size(321, 20);
+            this.tbLatLon.TabIndex = 3;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(13, 123);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(323, 85);
+            this.tbResult.TabIndex = 4;
+            // 
+            // btRead
+            // 
+            this.btRead.Location = new System.Drawing.Point(15, 95);
+            this.btRead.Name = "btRead";
+            this.btRead.Size = new System.Drawing.Size(75, 23);
+            this.btRead.TabIndex = 5;
+            this.btRead.Text = "Read";
+            this.btRead.UseVisualStyleBackColor = true;
+            this.btRead.Click += new System.EventHandler(this.btRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 220);
+            this.Controls.Add(this.btRead);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbLatLon);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.TextBox tbLatLon;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btRead;
+    }
+}
+
---
+++ ReadTifFile/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OSGeo.GDAL;
+
+namespace ReadTifFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // init gdal -- very importance
+            GdalConfiguration.ConfigureGdal();
+            Gdal.AllRegister();
+        }
+
+        // Open tif file
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Filter = "Tif files (*.tif) | *.tif";
+            if(od.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = od.FileName;
+            }
+        }
+
+        // Read raster data from tif file
+        private void btRead_Click(object sender, EventArgs e)
+        {
+            // open tif file
+            using (Dataset img = Gdal.Open(tbPath.Text, Access.GA_ReadOnly))
+            {
+                // Get 3 bands of tif file
+                Band band1 = img.GetRasterBand(1);
+                Band band2 = img.GetRasterBand(2);
+                Band band3 = img.GetRasterBand(3);
+
+                // Get with and high of tif file in pixel
+                int w = band1.XSize;
+                int h = band1.YSize;
+
+                // Get tif transform
+                double[] t = new double[6];
+                img.GetGeoTransform(t);
+
+                // Get lat lon from interface
+                double lat = double.Parse(tbLatLon.Text.Split(' ')[0]);
+                double lon = double.Parse(tbLatLon.Text.Split(' ')[1]);
+
+                // Get x, y in pixel from lat, lon
+                int x = (int)Math.Round(((lat - t[0]) * (t[1] * t[5] - t[2] * t[4]) - t[2] * ((lon - t[3]) * t[1] - t[4] * (lat - t[0]))) 
+                    / (t[1] * (t[1] * t[5] - t[2] * t[4])), 0);
+                int y = (int)Math.Round(((lon - t[3]) * t[1] + t[4] * (lat - t[0])) / (t[1] * t[5] - t[2] * t[4]), 0);
+
+                // Read data
+                int[] by1 = new int[w];
+                int[] by2 = new int[w];
+                int[] by3 = new int[w];
+                band1.ReadRaster(0, y, w, 1, by1, w, 1, 0, 0);
+                band2.ReadRaster(0, y, w, 1, by2, w, 1, 0, 0);
+                band3.ReadRaster(0, y, w, 1, by3, w, 1, 0, 0);
+                int valueBand1 = by1[x];
+                int valueBand2 = by2[x];
+                int valueBand3 = by3[x];
+
+                // Show result
+                tbResult.Text = valueBand1.ToString() + " "
+                    + valueBand2.ToString() + " "
+                    + valueBand3.ToString();
+            }
+        }
+    }
+}
